7e3ba851375d81f58847db08ee651be88e6a6bb0
Hjalte Nannestad <hjalte.nannestad@gmail.com>
2024-08-08 03:01:29 +0200

    Update lute

--- default.nix
+++ default.nix
@@ -1,14 +1,24 @@
 { pkgs, lute }:
 
 pkgs.stdenv.mkDerivation {
-  name = "hjaltes-widgets";
+  name = "cvec";
   version = "0.1.0";
-
   src = ./.;
+
+  outputs = [ "out" "dev" ];
 
   nativeBuildInputs = [
     pkgs.clang-tools
     pkgs.clang
+    pkgs.pkg-config
     lute.packages.${pkgs.system}.lute
   ];
+
+  buildPhase = ''
+    lute build cvec --release
+  '';
+
+  installPhase = ''
+    lute install cvec --nix
+  '';
 }
